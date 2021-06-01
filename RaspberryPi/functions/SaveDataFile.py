7c7ad64a2bc5864fa388055da8b228ed7fb9bc11
AdmHatena <33020756+AdmHatena@users.noreply.github.com>
2021-06-01 22:42:26 +0900

    add AESCipher.py (mv&fix)

---
+++ RaspberryPi/functions/SaveDataFile.py
@@ -0,0 +1,11 @@
+# ----- SaveDataFile ----- #
+# written by AdmHatena(https://github.com/AdmHatena/)
+# version 3.9.0 64-bit
+
+import AESCipher
+
+class SaveDataFile:
+    def read(path:str):
+        pass
+    def write(dat:object, path:str):
+        pass
