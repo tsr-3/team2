d4e42a1aa698a96a32ca9733fc6ba1f781fd3acd
AdmHatena <33020756+AdmHatena@users.noreply.github.com>
2021-06-02 20:05:05 +0900

    iroiro

--- RaspberryPi/functions/SaveDataFile.py
+++ RaspberryPi/functions/SaveDataFile.py
@@ -6,6 +6,8 @@ import AESCipher
 
 class SaveDataFile:
     def read(path:str):
+        with open(path, 'r', 1, 'utf-8') as fp:
+            encoded = fp.read()
         pass
     def write(dat:object, path:str):
         pass
--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -1,8 +1,17 @@
 # --- RaspberryPi main script --- #
 
+import datetime
+import dateutil.praser
+
 # -- main function -- #
 def main():
-    pass
+    # read save file
+    while True:
+        idm = cardreader()
+        result = comp(idm, students, lect_time)
+        # comp(idm:str, students:[str], lect_time:{start:datetime,end:datetime,late:datetime})
+        # monitor
+        # datawrite()
 
 # -- onexec -- #
 if __name__ == '__main__':
