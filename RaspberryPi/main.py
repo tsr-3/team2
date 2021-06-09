0d03ede09f4f393baee9d1b4ddaf6fdc193391a3
AdmHatena <33020756+AdmHatena@users.noreply.github.com>
2021-06-10 01:43:54 +0900

    add data.txt

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -4,7 +4,7 @@ import datetime
 import dateutil.praser
 
 # --- import functions --- #
-from ./function/time_re.py import time # これどうやってやるん
+from .functions import time_re as time # これどうやってやるん => できたわ
 
 # -- main function -- #
 def main():
@@ -14,7 +14,7 @@ def main():
     # read save file
     while True:
         idm, result['time'] = cardreader()
-        result['attendance'] = time(result['time'], lect_dat['time'])
+        result['attendance'] = time.my_time(result['time'], lect_dat['time'])
         result['inStudent'] = studentof(idm, lect_dat['students']['idms'])
         # comp(idm:str, students:[str], lect_time:{start:datetime,end:datetime,late:datetime})
         # monitor
