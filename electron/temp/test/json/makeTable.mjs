6617e94f7ba052a42090d5db304bd09ce1ff18a8
tsr <82254860+tsr-on-github@users.noreply.github.com>
2021-06-14 17:00:03 +0900

    動かん

--- electron/temp/test/json/makeTable.mjs
+++ electron/temp/test/json/makeTable.mjs
@@ -1,4 +1,25 @@
 function maketable(gakusei_list){
   // この部分に表を作るやつ書いて
   // jsonから読み取ったobjectはgakusei_listに入れたから
+  let body = document.getElementByIdTagName("body");
+
+  let tbl = document.createElement("table");
+  let tblBody = document.createElement("tbody");
+
+  for (let i = 0; i < gakusei_list.length; i++){
+    let row = document.createElement("tr");
+
+    for (let j = 0; j < gakusei_list[0].length; j++){
+      let cell = document.createElement("td");
+      let cellText = document.createTextNode(gakusei_list[i][j]);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+
+    tblBody.appendChild(row);
+  }
+
+  tbl.appendChild(tblBody);
+  body.appendChild(tbl);
+  tbl.setAttribute("border", "2");
 }
