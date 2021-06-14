98d867fc4f67fc1606518b8130b37ae7436ad6b6
tsr <tashiro1120@outlook.jp>
2021-06-14 23:47:39 +0900

    動くやつ

--- electron/temp/test/json/makeTable.mjs
+++ electron/temp/test/json/makeTable.mjs
@@ -1,25 +1,35 @@
 function maketable(gakusei_list){
   // この部分に表を作るやつ書いて
   // jsonから読み取ったobjectはgakusei_listに入れたから
-  let body = document.getElementByIdTagName("body");
+  let body = document.querySelector("body");//bodyに入れたい
 
-  let tbl = document.createElement("table");
-  let tblBody = document.createElement("tbody");
+  let table = document.createElement("table");
+  let tableBody = document.createElement("tbody");
+  //console.log(gakusei_list.length);
+
+  //console.log(gakusei_list)
 
   for (let i = 0; i < gakusei_list.length; i++){
     let row = document.createElement("tr");
 
-    for (let j = 0; j < gakusei_list[0].length; j++){
+    let object_keys = Object.keys(gakusei_list[i]);
+    //console.log(object_keys);//1からn人までの数値
+
+    for (let j = 0; j < object_keys.length; j++){
+      let object_keys_keys = object_keys[j];//各キーの中のキーを取得
+      console.log(object_keys_keys); // => 学籍番号，名前，ふりがな，性別，IDm が出る
+      //console.log(gakusei_list[i].object_keys_keys);
+
       let cell = document.createElement("td");
-      let cellText = document.createTextNode(gakusei_list[i][j]);
+      let cellText = document.createTextNode(gakusei_list[i][object_keys_keys]);
       cell.appendChild(cellText);
       row.appendChild(cell);
     }
 
-    tblBody.appendChild(row);
+    tableBody.appendChild(row);
   }
 
-  tbl.appendChild(tblBody);
-  body.appendChild(tbl);
-  tbl.setAttribute("border", "2");
+  table.appendChild(tableBody);
+  body.appendChild(table);
+  table.setAttribute("border", "2");
 }
