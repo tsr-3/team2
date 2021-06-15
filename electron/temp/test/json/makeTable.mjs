813a2ac52628ae1e49439482edf37f8555007865
tsr <tashiro1120@outlook.jp>
2021-06-15 18:10:47 +0900

    コメントの追加

--- electron/temp/test/json/makeTable.mjs
+++ electron/temp/test/json/makeTable.mjs
@@ -1,21 +1,23 @@
 function maketable(append_list,filename){
-  // この部分に表を作るやつ書いて
-  // jsonから読み取ったobjectはgakusei_listに入れたから
+  // この部分に表を作るやつ書いて -> 書いた
+  // jsonから読み取ったobjectはgakusei_listに入れたから ->　append_list(和訳：出席リスト)とfilenameを入れるように改造したぞ(index.mjsの方も)
 
-  //オブジェクトのキー(gakusei_listの場合)
-  let object_keys = Object.keys(append_list[0]);//この書き方よろしくない気がする
+  //オブジェクトのキーの中のキー(gakusei_listの場合，学籍番号，名前，ふりがな，性別，IDm)を取得
+  let object_keys = Object.keys(append_list[0]);//この書き方よろしくない気がするのでhatena氏のコメント待ち
 
-  let body = document.querySelector("body");//bodyに入れたい
+  let body = document.querySelector("body");//<body></body>に表を入れる
 
-  let table = document.createElement("table");
-  let caption = document.createElement("caption");
-  let table_title = document.createTextNode(filename);
+  let table = document.createElement("table");//<table>を作成
+  let caption = document.createElement("caption");//<caption>を作成
+  let table_title = document.createTextNode(filename);//<caption>filename</caption>にする
   caption.appendChild(table_title);
   table.appendChild(caption);
-  let thead = document.createElement("thead");
-  let tr = document.createElement("tr");
+
+  //オブジェクトのキーを見出しにする処理
+  let thead = document.createElement("thead");//<thead>を作成
+  let tr = document.createElement("tr");//<tr>を作成
   for (let i = 0; i < object_keys.length; i++){
-    let th = document.createElement("th");
+    let th = document.createElement("th");//<th>を作成
     let subtitle = document.createTextNode(object_keys[i]);
     th.appendChild(subtitle);
     tr.appendChild(th);
@@ -23,7 +25,9 @@ function maketable(append_list,filename){
   thead.appendChild(tr);
   table.appendChild(thead);
   let tableBody = document.createElement("tbody");
+  //見出しの作成終了
 
+  //ここから内容の描画だよ
   for (let i = 0; i < append_list.length; i++){
     let row = document.createElement("tr");
 
@@ -36,8 +40,9 @@ function maketable(append_list,filename){
 
     tableBody.appendChild(row);
   }
-
   table.appendChild(tableBody);
   body.appendChild(table);
-  table.setAttribute("border", "1");
+  //内容の表示終了
+
+  table.setAttribute("border", "1");//よく分からんけど，表とページの境界の線を設定できるらしい
 }
