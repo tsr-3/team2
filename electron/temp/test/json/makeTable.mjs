f2c91120b0c8bfef476ffe0e2885d4d19bd49002
tsr <82254860+tsr-on-github@users.noreply.github.com>
2021-06-15 14:13:02 +0900

    ファイルパスからファイル名だけを取得したかった

--- electron/temp/test/json/makeTable.mjs
+++ electron/temp/test/json/makeTable.mjs
@@ -1,15 +1,15 @@
-function maketable(gakusei_list){
+function maketable(append_list,filename){
   // この部分に表を作るやつ書いて
   // jsonから読み取ったobjectはgakusei_listに入れたから
 
   //オブジェクトのキー(gakusei_listの場合)
-  let object_keys = Object.keys(gakusei_list[0]);//この書き方よろしくない気がする
+  let object_keys = Object.keys(append_list[0]);//この書き方よろしくない気がする
 
   let body = document.querySelector("body");//bodyに入れたい
 
   let table = document.createElement("table");
   let caption = document.createElement("caption");
-  let table_title = document.createTextNode("題名(ファイル名とかにできないかな？)");
+  let table_title = document.createTextNode(filename);
   caption.appendChild(table_title);
   table.appendChild(caption);
   let thead = document.createElement("thead");
@@ -24,12 +24,12 @@ function maketable(gakusei_list){
   table.appendChild(thead);
   let tableBody = document.createElement("tbody");
 
-  for (let i = 0; i < gakusei_list.length; i++){
+  for (let i = 0; i < append_list.length; i++){
     let row = document.createElement("tr");
 
     for (let j = 0; j < object_keys.length; j++){
       let cell = document.createElement("td");
-      let cellText = document.createTextNode(gakusei_list[i][object_keys[j]]);
+      let cellText = document.createTextNode(append_list[i][object_keys[j]]);
       cell.appendChild(cellText);
       row.appendChild(cell);
     }
