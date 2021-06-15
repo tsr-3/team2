7f0964d06763454eca579519980c40459fcd8bd8
tsr <82254860+tsr-on-github@users.noreply.github.com>
2021-06-15 13:30:21 +0900

    題名と小見出しの追加

--- electron/temp/test/json/makeTable.mjs
+++ electron/temp/test/json/makeTable.mjs
@@ -1,12 +1,28 @@
 function maketable(gakusei_list){
   // この部分に表を作るやつ書いて
   // jsonから読み取ったobjectはgakusei_listに入れたから
+
+  //オブジェクトのキー(gakusei_listの場合)
+  let object_keys = Object.keys(gakusei_list[0]);//この書き方よろしくない気がする
+
   let body = document.querySelector("body");//bodyに入れたい
 
   let table = document.createElement("table");
+  let caption = document.createElement("caption");
+  let table_title = document.createTextNode("題名(ファイル名とかにできないかな？)");
+  caption.appendChild(table_title);
+  table.appendChild(caption);
+  let thead = document.createElement("thead");
+  let tr = document.createElement("tr");
+  for (let i = 0; i < object_keys.length; i++){
+    let th = document.createElement("th");
+    let subtitle = document.createTextNode(object_keys[i]);
+    th.appendChild(subtitle);
+    tr.appendChild(th);
+  }
+  thead.appendChild(tr);
+  table.appendChild(thead);
   let tableBody = document.createElement("tbody");
-
-  let object_keys = Object.keys(gakusei_list[0]);//この書き方よろしくない気がする
 
   for (let i = 0; i < gakusei_list.length; i++){
     let row = document.createElement("tr");
@@ -23,5 +39,5 @@ function maketable(gakusei_list){
 
   table.appendChild(tableBody);
   body.appendChild(table);
-  table.setAttribute("border", "2");
+  table.setAttribute("border", "1");
 }
