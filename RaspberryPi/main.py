4d4095705ce38ba9762a07e4df99ce84881049bc
abava00 <60212516+abava00@users.noreply.github.com>
2021-06-17 15:36:39 +0900

    Main.py からGUIを 呼び出せるように実装

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -1,18 +1,41 @@
 # --- RaspberryPi main script --- #
 
 import datetime
-import dateutil.praser
+import dateutil.parser
+
+#GUI表示に必要なもの
+from gui import windowsv3   #./gui/windowsv3 をimportしている
+import sys
+from PyQt5.QtWidgets import *
+
+
+import threading
+import concurrent.futures
+import time
 
 # -- main function -- #
 def main():
     # read save file
-    while True:
-        idm = cardreader()
-        result = comp(idm, students, lect_time)
-        # comp(idm:str, students:[str], lect_time:{start:datetime,end:datetime,late:datetime})
+    for i in range(10):
+
+        #idm = cardreader()
+        #result = comp(idm students, lect_time)
+         #comp(idm:str, students:[str], lect_time:{start:datetime,end:datetime,late:datetime})
         # monitor
-        # datawrite()
+         #datawrite()
+        print("Done mainfunc")
 
 # -- onexec -- #
 if __name__ == '__main__':
-    main()
+
+
+    # スレッド(max_worker の数だけ同時処理が出来ます)
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+    # 繰り返し行う処理
+    executor.submit(main)
+
+    print("hello world")
+
+    # GUIの起動
+    app=QApplication(sys.argv)
+    win = windowsv3.WinMake(app)
