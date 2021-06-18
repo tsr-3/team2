d7e98eff99deafb5b20a400ee4e66fbdfe964bdb
tsr <82254860+tsr-on-github@users.noreply.github.com>
2021-06-18 13:58:31 +0900

    関数化 def してください

--- RaspberryPi/functions/comparsion.py
+++ RaspberryPi/functions/comparsion.py
@@ -6,7 +6,7 @@ from types import LambdaType
 import datetime
 import csv
 #def comp(idm:str,students,lect_time):
-    
+
 time = datetime.datetime.now()
 print(time)
 #time ="06/02 15:15"
@@ -17,6 +17,8 @@ late_time=datetime.datetime(2021,6,2,18,00)
 idm=str('a012')
 #students=('b012')
 
+#returnするなら関数化しましょう
+
 with open('学生リスト.csv',newline='') as f:
     reader=csv.DictReader(f)
     for row in reader:
@@ -24,5 +26,5 @@ with open('学生リスト.csv',newline='') as f:
             print('履修者ではありません')
         number=(row['学籍番号'])
 return number
-        
+
 
