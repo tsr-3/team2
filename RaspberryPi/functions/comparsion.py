2b86e997778ca0c2cdd24e66b0f7a061adfc7104
Miku0627 <84836325+Miku0627@users.noreply.github.com>
2021-06-21 14:11:00 +0900

    Add files via upload
    訂正版

--- RaspberryPi/functions/comparsion.py
+++ RaspberryPi/functions/comparsion.py
@@ -1,28 +1,19 @@
-#学生リスト.csvから
-#
-
+'''
+入力：studentID
+出力：studentName
+説明：学籍番号が入力されたらそれに対応する学生の氏名を返す関数
+'''
 
 from types import LambdaType
-import datetime
-import csv
-#def comp(idm:str,students,lect_time):
-    
-time = datetime.datetime.now()
-print(time)
-#time ="06/02 15:15"
-start_time =datetime.datetime(2021,6,2,16,00)
-end_time =datetime.datetime(2021,6,2,19,00)
-late_time=datetime.datetime(2021,6,2,18,00)
 
-idm=str('a012')
-#students=('b012')
+import json
 
-with open('学生リスト.csv',newline='') as f:
-    reader=csv.DictReader(f)
-    for row in reader:
-        if idm!=row['IDm']:
-            print('履修者ではありません')
-        number=(row['学籍番号'])
-return number
+def comp(studentID,studentName)
+    json_open=open('学生リスト.json','r')
+    json_load=json.load(json_open)
+    studentID=[S001,S002,...,S100]
+    for v in json.load.values():
+        if studentID==v['学籍番号']:
+            studentName=v['名前']
+            return studentName
         
-
