3ab1ee878c14ca3de16314562bd5066e311d3dce
tsr <82254860+tsr-on-github@users.noreply.github.com>
2021-06-25 22:45:48 +0900

    コメントの改良

--- electron/tmp-app/createNewWindow.js
+++ electron/tmp-app/createNewWindow.js
@@ -1,10 +1,15 @@
+/**
+ * @param filepath:str
+ * @return {undefined}
+ */
 /*
   入力：ファイルの相対パスを文字列で入力
   出力：別ウィンドウで指定されたページを開く
   説明：新しいウィンドウを作成して，指定されたページを開くスクリプト
+
 */
 
-function openNewWindow(url) {
+function openNewWindow(filepath) {
   //const url = 'newWindowTest.html'
-  window.open(url, '', { width: 800, height: 600 })
+  window.open(filepath, '', { width: 800, height: 600 })
 }
