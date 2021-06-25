cbef693b273c0d7e227863997cee262863c22d38
tsr <82254860+tsr-on-github@users.noreply.github.com>
2021-06-25 23:45:06 +0900

    グラフ描画するやつ作成中

--- electron/tmp-app/makeGraph.mjs
+++ electron/tmp-app/makeGraph.mjs
@@ -1,12 +1,29 @@
 /**
  * make graph
- * @param {[{student: string, ratio: number}]} arr
+ * @param {[{student: string, ratio: number}]} attenddata
+ * @param filename:str
  * @return {undefined}
  */
-function make_graph(arr) {
-  let hr = document.querySelectorAll("hr")[1];
-  let canvas = document.createElement("canvas");
+function make_graph(attenddata, filename) {
+  let hr = document.querySelectorAll("hr")[0];
+  let figure = document.createElement("figure");
+  figure.className = 'graph';
+  let figcaption = document.createElement("figcaption");
+  figcaption.className = 'graph-caption';
+  figcaption.textContent = filename;
+  figure.appendChild(figcaption);
 
+  //テスト中
+  let ul = document.createElement("ul");
+  for (let i = 0; i < 10; i++){
+    let li = document.createElement("li");
+    for (let j = 0; j < 10; j++){
+
+    }
+  }
+
+
+  hr.appendChild(figure);
 }
 
 
