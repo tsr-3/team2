77ec21f6c1333a1fca9dc1ad8295cca09f720746
abava00 <60212516+abava00@users.noreply.github.com>
2021-07-05 00:30:43 +0900

    add comment

--- RaspberryPi/functions/dummy_cardreader.py
+++ RaspberryPi/functions/dummy_cardreader.py
@@ -1,3 +1,17 @@
+'''
+製作者:B21P036
+NFCが無い環境でもデバック作業が行えるように,IDmを標準入力によって入力するプログラム
+
+on_connect():IDmを標準入力として取得する
+GOon_connect
+
+read_id():互換性の為に経由するする
+GOread_id
+
+printidm():IDmを返す (PrintなんだからPrintしろ)
+GOprintidm
+
+'''
 import binascii
 from time import time
 import sys
@@ -7,17 +21,19 @@ import datetime#時間のインポート
 # -- global --
 idm= ""
 
-
+#GOon_connect
 def on_connect():
     print("touched")
     global idm
     idm = input("Your ID:")
     return True
 
+#GOread_id
 def read_id():
     on_connect()
 
 
+#GOprintidm
 def printidm():
     read_id()
     now= datetime.datetime.now()
