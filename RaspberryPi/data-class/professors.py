a7290c84b2499c3eee5b0dfbd868cd53c6622f41
tsr <82254860+tsr-on-github@users.noreply.github.com>
2021-07-10 01:08:58 +0900

    デバッグはまだらしい

--- RaspberryPi/data-class/professors.py
+++ RaspberryPi/data-class/professors.py
@@ -1,6 +1,5 @@
-
 class Professors:
-
+    
     _professors:list = None
 
     # Professors() と呼び出したとき
@@ -8,8 +7,14 @@ class Professors:
     # Professors(value) と呼び出したとき
     #   self._professorsにvalueを設定する
     #   valueリストの各データにid, name, yomi, sex, lectのキーが存在することをチェックし、一つでもキーが存在しなかったらExceptionを投げる
-    def __init__(self, value:list = None):
+
+    def __init__(self, value:list = None):#コンストラクタ
         pass
+        self._professors==None
+        if type(value) is list:
+            if value in value ('id')or('name')or('yomi')or('sex')or('lect[]'):
+                self._professors==self._professors(value)
+            return self._professors    
 
     # keyがintのとき
     #   self._professorsのリストのkey番目の教員データを返す
@@ -19,20 +24,39 @@ class Professors:
     #   strの教員IDが存在しないとき、Noneを返す
     # keyがそれ以外の時
     #   Exceptionを投げる
+    """
     def __getitem__(self, key):
         pass
+        for key in self._prfessors():
+            if type(key)==int:
+                return self._professors(key)
+                if self._professors[key]>=self._professors():
+                    return None
+            if type(key)==str:
+                self._professors(id)==self._professors('')
+                return self._professors(id)
+                if self._professors('id')=='':
+                    return None
+            else:
+                return Exception
 
     @property
     def count(self):
         pass
+        for key in self._professors():
+            counter+=1
+        return counter
     # 教員データの数を返す
     @count.getter
     def count(self):
         pass
+        count():
+        return counter
 
     @property
     def data(self):
         pass
+        return self._professors()
     # 教員データのリストを返す
     @data.getter
     def data(self):
@@ -50,89 +74,10 @@ class Professors:
     @empty.getter
     def empty(self):
         pass
+"""
 
-
-
-# 新しい方
-class Professors:
-    def __init__(self,value,professors,id,name,yomi,sex,lect,):
-        _professors:list = None
-
-Professors()
-    def __init__(self,value,professors,id,name,yomi,sex,lect,):
-        self._professors=None
-        Professors(value)
-        self._professors=value
-        #   valueリストの各データにid, name, yomi, sex, lectのキーが存在することをチェックし、一つでもキーが存在しなかったらExceptionを投げる
-        for i in range(professors(value)):
-            if id!='':
-                 Exception
-            if name!='':
-                 Exception
-            if yomi!='':
-                 Exception
-            if sex!='':
-                 Exception
-            if lect!='':
-                 Exception
-
-
-    def __init__(self, value:list = None):
-        pass
-    for list in list:
-        if type(list)==int:
-            for key in range(self._professors()):
-                if key>=data:
-                    return 'None'
-                return data
-        if type(list)==str:
-            id==data[key]
-            if id!='':
-                Exception
-        else:
-            Exception
-
-
-
-
-    # keyがintのとき
-    #   self._professorsのリストのkey番目の教員データを返す
-    #   self._professors[key]がリストの範囲外のとき、Noneを返す
-    # keyがstrのとき
-    #   教員IDがstrの教員データを返す
-    #   strの教員IDが存在しないとき、Noneを返す
-    # keyがそれ以外の時
-    #   Exceptionを投げる
-
-    def __getitem__(self, key):
-        pass
-
-    @property
-    def count(self):
-        pass
-
-    # 教員データの数を返す
-    @count.getter
-    def count(self):
-        pass
-
-    @property
-    def data(self):
-        pass
-    # 教員データのリストを返す
-    @data.getter
-    def data(self):
-        pass
-    # self._professorsにvalueを設定する、同時にコンストラクタでも行ったキーのチェックも行う
-    @data.setter
-    def data(self, value):
-        pass
-
-    @property
-    def empty(self):
-        pass
-    # データが設定されていないかどうかを返す
-    # => データが設定されていないときにtrue
-    @empty.getter
-    def empty(self):
-        pass
+if __name__=='__main__':
+    value=[]
+    if value==('id')or('name')or('yomi')or('sex')or('lect[]'):
+        print('Exception')
+    
