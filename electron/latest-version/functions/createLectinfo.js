70337e0c177553553699150828d2b7c008082f18
tsr <82254860+tsr-on-github@users.noreply.github.com>
2021-07-20 02:54:46 +0900

    途中

--- electron/latest-version/functions/createLectinfo.js
+++ electron/latest-version/functions/createLectinfo.js
@@ -18,13 +18,35 @@ document.querySelector('#csv-reader').addEventListener('change', async (event)=>
   reader.readAsText(event.target.files[0]);
 });
 
-async function createLecttime() {
+function csv2json(csvArray){
+  let jsonArray = [];
+
+  // 1行目から「項目名」の配列を生成する
+  let items = ['id', 'name', 'yomi', 'sex', 'IDm'];
+
+  // CSVデータの配列の各行をループ処理する
+  // 配列の先頭要素(行)は項目名のため処理対象外
+  // 配列の最終要素(行)は空のため処理対象外
+  for (let i = 1; i < csvArray.length; i++) {
+    let a_line = new Object();
+    // カンマで区切られた各データに分割する
+    let csvArrayD = csvArray[i].split(',');
+    // 各データをループ処理する
+    for (let j = 0; j < items.length; j++) {
+      // 要素名：items[j]
+      // データ：csvArrayD[j]
+      a_line[items[j]] = csvArrayD[j];
+    }
+    jsonArray.push(a_line);
+  }
+  return jsonArray;
+}
+
+function getLecttime() {
   let starttime = document.querySelector('form > input#starttime').value;
   let attendtime = document.querySelector('form > input#attendtime').value;
   let latenesstime = document.querySelector('form > input#latenesstime').value;
 
-  csvFileReader();
-  // テスト用
   // console.log() 部分は後で createElement にする
   if (starttime == null || starttime == undefined || starttime == '') {
     console.log("講義開始時間は入力しよう...うん...");
@@ -43,30 +65,21 @@ async function createLecttime() {
   }
 
   let lecttime = { 'start': starttime, 'attend': attendtime, 'lateness': latenesstime }; // 講義に関する時間
-
   return lecttime
 };
 
-function csv2json(csvArray){
-  let jsonArray = [];
+function createLectInfo() {
+  let lect_info = {}
+  // 履修者とかのデータをまとめる処理
+  let lecttime = getLecttime();
 
-  // 1行目から「項目名」の配列を生成する
-  let items = csvArray[0].split(',');
 
-  // CSVデータの配列の各行をループ処理する
-  // 配列の先頭要素(行)は項目名のため処理対象外
-  // 配列の最終要素(行)は空のため処理対象外
-  for (let i = 1; i < csvArray.length; i++) {
-    let a_line = new Object();
-    // カンマで区切られた各データに分割する
-    let csvArrayD = csvArray[i].split(',');
-    // 各データをループ処理する
-    for (let j = 0; j < items.length; j++) {
-      // 要素名：items[j]
-      // データ：csvArrayD[j]
-      a_line[items[j]] = csvArrayD[j];
-    }
-    jsonArray.push(a_line);
-  }
-  return jsonArray;
+
+  let filename = '○○さんのデータ'
+  const blob = new Blob([JSON.stringify(lect_info, null, 2)], { type: 'application/json' });
+  let link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = filename+'.json';
+  link.click();
+  return;
 }
