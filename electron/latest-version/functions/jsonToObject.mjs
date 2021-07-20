0fe4a9d3f471d55dcadfc699daf4e7b7e9126861
tsr <82254860+tsr-on-github@users.noreply.github.com>
2021-07-20 22:57:32 +0900

    動かん

--- electron/latest-version/functions/jsonToObject.mjs
+++ electron/latest-version/functions/jsonToObject.mjs
@@ -1,24 +1,20 @@
-/*
-  jsonファイルをオブジェクトに変換する関数
-*/
-
 var global = {};
 
-import * as SaveDataFile from '../functions/SaveDataFile.mjs';
+//import * as SaveDataFile from './SaveDataFile.mjs';
 
 // input file read
-document.querySelector('#json-reader').addEventListener('change', async (event)=>{
-  //console.log(event.path[0].value); // path
+document.querySelector('#student-reader').addEventListener('change', async (event) => {
+  console.log(event.path[0].value); // path
   if (!event.path[0].value.match(/.json$|.t2pecf$/)){ // is json
     document.querySelector('#filetype-check').innerText = 'invalid filetype "' + event.path[0].value.match(/\..+$/)[0] + '"';
     document.querySelector('#filetype-check').classList.add('error')
     return;
   }
   let filename = event.target.files[0].name;
-  var reader = new FileReader();
-  reader.onload = (event)=>{
+  let reader = new FileReader();
+  reader.onload = (event) => {
     global.json = event.target.result;
-    let data = SaveDataFile.parse(global.json);
+    let data = parse(global.json);
     maketable(data.attendance, filename);
     drawgraph(data.attendance, filename);
   };
