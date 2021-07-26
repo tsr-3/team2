e7d7ab0150556a7b9c3a1718ae2a4d8dbacdfa76
AdmHatena <33020756+AdmHatena@users.noreply.github.com>
2021-07-27 02:23:02 +0900

    remove in RaspberryPi

--- RaspberryPi/functions/test/crypto/cry_gen_key.py
+++
@@ -1,7 +0,0 @@
-import string, random
-
-def genKey():
-    return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(50)])
-
-if __name__ == '__main__':
-    print(genKey())
