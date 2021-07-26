0e6dc73bd4886c78c9eed603e0966101c20772e5
AdmHatena <33020756+AdmHatena@users.noreply.github.com>
2021-07-27 02:20:39 +0900

    make stable branch and rm on / (root)

--- pi-electron-communicate/AES256CBC/out.test.cjs
+++
@@ -1,7 +0,0 @@
-
-const {AES256CBC} = require('./AES256CBC.cjs');
-const fs = require('fs');
-
-let students_csv = fs.readFileSync('../../data/学生リスト.csv', 'utf-8');
-let professors_csv = fs.readFileSync('../../data/教員・担当科目リスト.csv', 'utf-8');
-
